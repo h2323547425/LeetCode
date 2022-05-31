31f3aea49cd4c12a62ea5310e63fea47443880c1
h2323547425 <hongyi_li@brown.edu>
2022-05-30 22:45:20 -0400

    add solution 250

--- src/Algo/Solution250.java
+++ src/Algo/Solution250.java
@@ -7,9 +7,24 @@ package Algo;
  * A uni-value subtree means all nodes of the subtree have the same value.
  */
 public class Solution250 {
+    int count = 0;
 
     public int countUnivalSubtrees(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        isUnivalSubtrees(root);
+        return count;
+    }
 
+    public boolean isUnivalSubtrees(TreeNode root) {
+        boolean left = root.left == null || (isUnivalSubtrees(root.left) && root.left.val == root.val);
+        boolean right = root.right == null || (isUnivalSubtrees(root.right) && root.right.val == root.val);
+        if (left && right ) {
+            count++;
+            return true;
+        }
+        return false;
     }
 
 }
--- src/Algo/TreeNode.java
+++ src/Algo/TreeNode.java
@@ -7,7 +7,6 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-
     TreeNode() {
     }
 
