9f9edbc1b54b8fd03985b2a4eecb561e86832bce
h2323547425 <hongyi_li@brown.edu>
2022-05-30 22:29:52 -0400

    add TreeNode implementation for problem 250

---
+++ src/Algo/Solution250.java
@@ -0,0 +1,15 @@
+package Algo;
+
+
+/**
+ * Given the root of a binary tree, return the number of uni-value subtrees.
+ *
+ * A uni-value subtree means all nodes of the subtree have the same value.
+ */
+public class Solution250 {
+
+    public int countUnivalSubtrees(TreeNode root) {
+
+    }
+
+}
