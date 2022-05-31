edeff1be00f1808bc2d32c28505d3f22c6dcf5ef
h2323547425 <hongyi_li@brown.edu>
2022-05-31 19:52:40 -0400

    add solution 252

---
+++ src/Algo/Solution252.java
@@ -0,0 +1,22 @@
+package Algo;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of meeting time intervals where intervals[i] =
+ * [starti, endi], determine if a person could attend all meetings.
+ */
+public class Solution252 {
+
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
