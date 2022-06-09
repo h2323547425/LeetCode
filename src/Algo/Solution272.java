eb03b1330a675d7d651685d090052294e3459676
h2323547425 <hongyi_li@brown.edu>
2022-06-08 21:17:43 -0400

    add 6/8 temp

---
+++ src/Algo/Solution272.java
@@ -0,0 +1,19 @@
+package Algo;
+
+import java.util.List;
+
+/**
+ * Given the root of a binary search tree, a target value, and an integer k,
+ * return the k values in the BST that are closest to the target. You may
+ * return the answer in any order.
+ *
+ * You are guaranteed to have only one unique set of k values in the BST
+ * that are closest to the target.
+ */
+public class Solution272 {
+
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+
+    }
+
+}
