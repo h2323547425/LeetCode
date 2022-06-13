6e1efd1b055ea8e6bf22f2fd4845e7a8266d3c87
h2323547425 <hongyi_li@brown.edu>
2022-06-12 21:25:21 -0400

    add partial 6/12

---
+++ src/Algo/Solution281.java
@@ -0,0 +1,42 @@
+package Algo;
+
+import java.util.List;
+
+/**
+ * Given two vectors of integers v1 and v2, implement an iterator
+ * to return their elements alternately.
+ *
+ * Implement the ZigzagIterator class:
+ *
+ * ZigzagIterator(List<int> v1, List<int> v2) initializes the object
+ * with the two vectors v1 and v2.
+ *
+ * boolean hasNext() returns true if the iterator still has elements,
+ * and false otherwise.
+ *
+ * int next() returns the current element of the iterator and moves
+ * the iterator to the next element.
+ */
+public class Solution281 {
+
+    /**
+     * Your ZigzagIterator object will be instantiated and called as such:
+     * ZigzagIterator i = new ZigzagIterator(v1, v2);
+     * while (i.hasNext()) v[f()] = i.next();
+     */
+    public static class ZigzagIterator {
+
+        public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+
+        }
+
+        public int next() {
+
+        }
+
+        public boolean hasNext() {
+
+        }
+    }
+
+}
