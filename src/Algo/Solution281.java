74fd761e89ec9b16f47eed45cb86019c624fa403
h2323547425 <hongyi_li@brown.edu>
2022-06-16 21:13:44 -0400

    add solution 281

--- src/Algo/Solution281.java
+++ src/Algo/Solution281.java
@@ -25,17 +25,33 @@ public class Solution281 {
      * while (i.hasNext()) v[f()] = i.next();
      */
     public static class ZigzagIterator {
+        List<Integer>[] vectors;
+        int[] indices;
+        int vec_ptr;
 
         public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-
+            this.vectors = new List[] { v1, v2 };
+            this.indices = new int[] { 0, 0 };
         }
 
         public int next() {
-
+            if (hasNext()) {
+                if (this.indices[this.vec_ptr]
+                    >= this.vectors[this.vec_ptr].size()) {
+                    this.vec_ptr = 1 - this.vec_ptr;
+                }
+                int ret = this.vectors[this.vec_ptr]
+                    .get(this.indices[this.vec_ptr]);
+                this.indices[this.vec_ptr]++;
+                this.vec_ptr = 1 - this.vec_ptr;
+                return ret;
+            }
+            return -1;
         }
 
         public boolean hasNext() {
-
+            return this.indices[0] < this.vectors[0].size()
+                || this.indices[1] < this.vectors[1].size();
         }
     }
 
